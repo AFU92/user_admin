cb0e09e1e4ee144e526ae277bc0c727b32867e53
Andrea Fuentes <andrea.fuentes@peiky.com>
2020-03-21 22:40:23 -0500

    implementing preferences models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  has_many :preferences
+
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, :last_name, :phone_number, :address, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_213013) do
+ActiveRecord::Schema.define(version: 2020_03_21_043947) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "preferences", force: :cascade do |t|
+    t.string "description"
+    t.string "data", array: true
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_preferences_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_03_18_213013) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "preferences", "users"
 end
